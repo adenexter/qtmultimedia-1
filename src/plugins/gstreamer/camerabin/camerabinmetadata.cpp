2cead632fad78120b302c40a1a11e07045aa40e4
Andrew den Exter <andrew.den.exter@jollamobile.com>
2013-12-05 17:30:04 +1000

    Fix writing orientation and date exif tags in camerabin.
    Orientation tags need to be transformed from the string tag returned
    by gstreamer to the orientation in degrees.  Date tags need to be
    inserted with gst_date_time_new_local_time.  Finally setting a tag
    value shouldn't clear all other tags.
    
    Change-Id: I28922148251084c12cf6c93d9b097fa5df41da9d

--- src/plugins/gstreamer/camerabin/camerabinmetadata.cpp
+++ src/plugins/gstreamer/camerabin/camerabinmetadata.cpp
@@ -52,75 +52,110 @@ struct QGstreamerMetaDataKeyLookup
 {
     QString key;
     const char *token;
+    QVariant (*fromGStreamer)(const QVariant &value);
+    QVariant (*toGStreamer)(const QVariant &value);
 };
+
+static QVariant fromGStreamerOrientation(const QVariant &value)
+{
+    // Note gstreamer tokens either describe the counter clockwise rotation of the
+    // image or the clockwise transform to apply to correct the image.  The orientation
+    // value returned is the clockwise rotation of the image.
+    const QString token = value.toString();
+    if (token == QStringLiteral("rotate-90"))
+        return 270;
+    else if (token == QStringLiteral("rotate-180"))
+        return 180;
+    else if (token == QStringLiteral("rotate-270"))
+        return 90;
+    else
+        return 0;
+}
+
+static QVariant toGStreamerOrientation(const QVariant &value)
+{
+    switch (value.toInt()) {
+    case 90:
+        return QStringLiteral("rotate-270");
+    case 180:
+        return QStringLiteral("rotate-180");
+    case 270:
+        return QStringLiteral("rotate-90");
+    default:
+        return QStringLiteral("rotate-0");
+    }
+}
 
 static const QGstreamerMetaDataKeyLookup qt_gstreamerMetaDataKeys[] =
 {
-    { QMediaMetaData::Title, GST_TAG_TITLE },
-    //{ QMediaMetaData::SubTitle, 0 },
-    //{ QMediaMetaData::Author, 0 },
-    { QMediaMetaData::Comment, GST_TAG_COMMENT },
-    { QMediaMetaData::Description, GST_TAG_DESCRIPTION },
-    //{ QMediaMetaData::Category, 0 },
-    { QMediaMetaData::Genre, GST_TAG_GENRE },
-    //{ QMediaMetaData::Year, 0 },
-    //{ QMediaMetaData::UserRating, 0 },
+    { QMediaMetaData::Title, GST_TAG_TITLE, 0, 0 },
+    //{ QMediaMetaData::SubTitle, 0, 0, 0 },
+    //{ QMediaMetaData::Author, 0,  0, 0 },
+    { QMediaMetaData::Comment, GST_TAG_COMMENT, 0, 0 },
+    { QMediaMetaData::Date, GST_TAG_DATE_TIME, 0, 0 },
+    { QMediaMetaData::Description, GST_TAG_DESCRIPTION, 0, 0 },
+    //{ QMediaMetaData::Category, 0, 0, 0 },
+    { QMediaMetaData::Genre, GST_TAG_GENRE, 0, 0 },
+    //{ QMediaMetaData::Year, 0, 0, 0 },
+    //{ QMediaMetaData::UserRating, 0, 0, 0 },
 
-    { QMediaMetaData::Language, GST_TAG_LANGUAGE_CODE },
+    { QMediaMetaData::Language, GST_TAG_LANGUAGE_CODE, 0, 0 },
 
-    { QMediaMetaData::Publisher, GST_TAG_ORGANIZATION },
-    { QMediaMetaData::Copyright, GST_TAG_COPYRIGHT },
-    //{ QMediaMetaData::ParentalRating, 0 },
-    //{ QMediaMetaData::RatingOrganisation, 0 },
+    { QMediaMetaData::Publisher, GST_TAG_ORGANIZATION, 0, 0 },
+    { QMediaMetaData::Copyright, GST_TAG_COPYRIGHT, 0, 0 },
+    //{ QMediaMetaData::ParentalRating, 0, 0, 0 },
+    //{ QMediaMetaData::RatingOrganisation, 0, 0, 0 },
 
     // Media
-    //{ QMediaMetaData::Size, 0 },
-    //{ QMediaMetaData::MediaType, 0 },
-    { QMediaMetaData::Duration, GST_TAG_DURATION },
+    //{ QMediaMetaData::Size, 0, 0, 0 },
+    //{ QMediaMetaData::MediaType, 0, 0, 0 },
+    { QMediaMetaData::Duration, GST_TAG_DURATION, 0, 0 },
 
     // Audio
-    { QMediaMetaData::AudioBitRate, GST_TAG_BITRATE },
-    { QMediaMetaData::AudioCodec, GST_TAG_AUDIO_CODEC },
-    //{ QMediaMetaData::ChannelCount, 0 },
-    //{ QMediaMetaData::SampleRate, 0 },
+    { QMediaMetaData::AudioBitRate, GST_TAG_BITRATE, 0, 0 },
+    { QMediaMetaData::AudioCodec, GST_TAG_AUDIO_CODEC, 0, 0 },
+    //{ QMediaMetaData::ChannelCount, 0, 0, 0 },
+    //{ QMediaMetaData::SampleRate, 0, 0, 0 },
 
     // Music
-    { QMediaMetaData::AlbumTitle, GST_TAG_ALBUM },
-    { QMediaMetaData::AlbumArtist,  GST_TAG_ARTIST},
-    { QMediaMetaData::ContributingArtist, GST_TAG_PERFORMER },
+    { QMediaMetaData::AlbumTitle, GST_TAG_ALBUM, 0, 0 },
+    { QMediaMetaData::AlbumArtist,  GST_TAG_ARTIST, 0, 0 },
+    { QMediaMetaData::ContributingArtist, GST_TAG_PERFORMER, 0, 0 },
 #if (GST_VERSION_MAJOR >= 0) && (GST_VERSION_MINOR >= 10) && (GST_VERSION_MICRO >= 19)
-    { QMediaMetaData::Composer, GST_TAG_COMPOSER },
+    { QMediaMetaData::Composer, GST_TAG_COMPOSER, 0, 0 },
 #endif
     //{ QMediaMetaData::Conductor, 0 },
     //{ QMediaMetaData::Lyrics, 0 },
     //{ QMediaMetaData::Mood, 0 },
-    { QMediaMetaData::TrackNumber, GST_TAG_TRACK_NUMBER },
+    { QMediaMetaData::TrackNumber, GST_TAG_TRACK_NUMBER, 0, 0 },
 
-    //{ QMediaMetaData::CoverArtUrlSmall, 0 },
-    //{ QMediaMetaData::CoverArtUrlLarge, 0 },
+    //{ QMediaMetaData::CoverArtUrlSmall, 0, 0, 0 },
+    //{ QMediaMetaData::CoverArtUrlLarge, 0, 0, 0 },
 
     // Image/Video
-    //{ QMediaMetaData::Resolution, 0 },
-    //{ QMediaMetaData::PixelAspectRatio, 0 },
+    //{ QMediaMetaData::Resolution, 0, 0, 0 },
+    //{ QMediaMetaData::PixelAspectRatio, 0, 0, 0 },
 
     // Video
-    //{ QMediaMetaData::VideoFrameRate, 0 },
-    //{ QMediaMetaData::VideoBitRate, 0 },
-    { QMediaMetaData::VideoCodec, GST_TAG_VIDEO_CODEC },
+    //{ QMediaMetaData::VideoFrameRate, 0, 0, 0 },
+    //{ QMediaMetaData::VideoBitRate, 0, 0, 0 },
+    { QMediaMetaData::VideoCodec, GST_TAG_VIDEO_CODEC, 0, 0 },
 
-    //{ QMediaMetaData::PosterUrl, 0 },
+    //{ QMediaMetaData::PosterUrl, 0, 0, 0 },
 
     // Movie
-    //{ QMediaMetaData::ChapterNumber, 0 },
-    //{ QMediaMetaData::Director, 0 },
-    { QMediaMetaData::LeadPerformer, GST_TAG_PERFORMER },
-    //{ QMediaMetaData::Writer, 0 },
+    //{ QMediaMetaData::ChapterNumber, 0, 0, 0 },
+    //{ QMediaMetaData::Director, 0, 0, 0 },
+    { QMediaMetaData::LeadPerformer, GST_TAG_PERFORMER, 0, 0 },
+    //{ QMediaMetaData::Writer, 0, 0, 0 },
 
     // Photos
-    //{ QMediaMetaData::CameraManufacturer, 0 },
-    //{ QMediaMetaData::CameraModel, 0 },
-    //{ QMediaMetaData::Event, 0 },
-    //{ QMediaMetaData::Subject, 0 }
+    //{ QMediaMetaData::CameraManufacturer, 0, 0, 0 },
+    //{ QMediaMetaData::CameraModel, 0, 0, 0 },
+    //{ QMediaMetaData::Event, 0, 0, 0 },
+    //{ QMediaMetaData::Subject, 0, 0, 0 }
+
+    { QMediaMetaData::Orientation, GST_TAG_IMAGE_ORIENTATION, &fromGStreamerOrientation, &toGStreamerOrientation }
 };
 
 CameraBinMetaData::CameraBinMetaData(QObject *parent)
@@ -133,10 +168,12 @@ QVariant CameraBinMetaData::metaData(const QString &key) const
     static const int count = sizeof(qt_gstreamerMetaDataKeys) / sizeof(QGstreamerMetaDataKeyLookup);
 
     for (int i = 0; i < count; ++i) {
-        if (qt_gstreamerMetaDataKeys[i].key == key) {
-            const char *name = qt_gstreamerMetaDataKeys[i].token;
+        const QGstreamerMetaDataKeyLookup &entry = qt_gstreamerMetaDataKeys[i];
+        if (entry.key == key) {
+            const char *name = entry.token;
 
-            return m_values.value(QByteArray::fromRawData(name, qstrlen(name)));
+            const QVariant value = m_values.value(QByteArray::fromRawData(name, qstrlen(name)));
+            return entry.fromGStreamer ? (*entry.fromGStreamer)(value) : value;
         }
     }
     return QVariant();
@@ -147,10 +184,13 @@ void CameraBinMetaData::setMetaData(const QString &key, const QVariant &value)
     static const int count = sizeof(qt_gstreamerMetaDataKeys) / sizeof(QGstreamerMetaDataKeyLookup);
 
     for (int i = 0; i < count; ++i) {
-        if (qt_gstreamerMetaDataKeys[i].key == key) {
-            const char *name = qt_gstreamerMetaDataKeys[i].token;
+        const QGstreamerMetaDataKeyLookup &entry = qt_gstreamerMetaDataKeys[i];
+        if (entry.key == key) {
+            const char *name = entry.token;
 
-            m_values.insert(QByteArray::fromRawData(name, qstrlen(name)), value);
+            m_values.insert(QByteArray::fromRawData(name, qstrlen(name)), entry.toGStreamer
+                        ? (*entry.toGStreamer)(value)
+                        : value);
 
             emit QMetaDataWriterControl::metaDataChanged();
             emit metaDataChanged(m_values);
